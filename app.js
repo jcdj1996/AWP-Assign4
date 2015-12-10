13005f09388af7fdc7762a900f2f84bbc3bde494
Amanda Field <afield788@gmail.com>
2015-12-10 01:41:52 -0500

    mongoLab

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ var mongoose = require('mongoose');
 var flash = require('connect-flash');
 var passport = require('passport');
 
-//DB Setup
+/*Local DB Setup
 var DB = require('./server/config/db.js');
 mongoose.connect(DB.url);
 mongoose.connection.on('error', function () {
   console.error('MongoDB Connection Error');
-});// live
-//mongoose.connect('mongodb://afield788:joey77@ds048368.mongolab.com:43027/assignment1');
+});// live */
+
+//connect to mongoLab account
+mongoose.connect('mongodb://afield:joey77@ds037814.mongolab.com:37814/heroku_gftj0f7r');
+
 
 // check connection
 var db = mongoose.connection;
@@ -104,3 +107,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+//implemented mongoLab db
