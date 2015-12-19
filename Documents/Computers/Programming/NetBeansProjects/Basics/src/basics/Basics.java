8bebb02f121d2b8cf59b004a7b433ea7dd5e10d9
Amanda Field <afield788@gmail.com>
2015-12-19 00:46:54 -0500

    push

--- Documents/Computers/Programming/NetBeansProjects/Basics/src/basics/Basics.java
+++
@@ -1,8 +0,0 @@
-package basics;
-
-
-public class Basics {
-
-
-   
-}
--- Documents/Computers/Programming/NetBeansProjects/JAVAMART_Wed/src/general/ServiceClass.java
+++
@@ -1,7 +0,0 @@
-package general;
-public class ServiceClass 
-{
-    private static int empId = 1000;
-    public static int getEmpId()
-    { return empId++; }
-}
